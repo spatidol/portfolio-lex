9179d0c0e9b63b308afada69e513b233f280b3b9
Allie Spatidol <spatidol@gmail.com>
2017-07-24 19:27:12 -0700

    decent project page

--- src/Web.js
+++ src/Web.js
@@ -4,8 +4,8 @@ import React from 'react';
 const Web = () => (
  <div className='web'>
    <div className='as-block'><img src="./images/artsc.png" alt='artschool' className='as'></img></div>
-   <div className='description float-left'><b>artschool</b> <br /> design // development <br /> a lifestyle brand & clothing line created by an art student <br /> html | css | javascript </div>
-   <div className='description float-left br'><b>brunch</b> <br /> design // development <br /> for your basic brunch fix <br /> application that returns all the brunch spots given a location search<br /> javascript | react | express | google api | html | css</div>
+   <div className='description float-left desc-as'><b>artschool</b> <br /> design // development <br /> a lifestyle brand & clothing line created by an art student <br /> html | css | javascript </div>
+   <div className='description float-left br'><b>brunch</b> <br /> design // development <br /> for your basic brunch fix <br /> application that returns all brunch spots given a location search<br /> javascript | react | express | google api | html | css</div>
    <div className='br-block'><div className='line'></div><img src="./images/brunch.png" alt='brunch' className='brunch'></img></div>
  </div>
 );
