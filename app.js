0b0a0a346a25907d53e94d06e559fe9a7b60d75a
Allie Spatidol <spatidol@gmail.com>
2017-08-03 10:23:39 -0700

    addview

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ var magnets = require('./routes/magnets');
 
 var app = express();
 
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'html');
 
 app.use('/magnets', magnets);
 
