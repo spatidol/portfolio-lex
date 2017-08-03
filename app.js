ce7f777eb9a82d718109e7db3ce44baa4d685f80
Allie Spatidol <spatidol@gmail.com>
2017-08-03 10:31:40 -0700

    wut

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ var magnets = require('./routes/magnets');
 
 var app = express();
 
-app.set('view engine', 'html');
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
