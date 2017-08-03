ddf9f3e51ddcfddd9df5fec9b33b8350c5db7882
Allie Spatidol <spatidol@gmail.com>
2017-08-03 13:12:40 -0700

    a

--- app.js
+++ app.js
@@ -13,9 +13,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/magnets', magnets);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
