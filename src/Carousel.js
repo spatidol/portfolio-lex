6cf5d1fab3f00c265702b1f776a758300a0c4340
Alexa Spatidol <Alexa@AMPASFC02XC55KJG5J.local>
2018-11-15 15:44:11 -0800

    carousel fix

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,50 +1,130 @@
 import React  from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
- 
-const HubbCarousel = () =>  (
-    <Carousel showThumbs={false} showStatus={false} emulateTouch={true} showArrows={false} centerMode={true} centerSlidePercentage={50} useKeyboardArrows={true}>
-        <div>
-            <img src="../img/mobile-mocks/hub-quiz-0.png" />
-        </div>
-        <div>
-            <img src="../img/mobile-mocks/hub-quiz-1.png" />
-        </div>
-    </Carousel> 
-);
 
-const HubCarousel = () =>  (
-    <Carousel showThumbs={false} showStatus={false} emulateTouch={true} showArrows={false} centerMode={true} centerSlidePercentage={50} useKeyboardArrows={true}>
-        <div>
-            <img src="../img/mobile-mocks/hub-0.png" />
-        </div>
-        <div>
-            <img src="../img/mobile-mocks/hub-1.png" />
-        </div>
-        <div>
-            <img src="../img/mobile-mocks/hub-2.png" />
-        </div>
-        <div>
-            <img src="../img/mobile-mocks/hub-3.png" />
-        </div>
-        <div>
-            <img src="../img/mobile-mocks/hub-4.png" />
-        </div>
-    </Carousel> 
-);
 
-const BaCarousel = () =>  (
-    <Carousel showThumbs={false} showStatus={false} emulateTouch={true} showArrows={false} centerMode={true} centerSlidePercentage={50} useKeyboardArrows={true}>
-        <div>
-            <img src="../img/mobile-mocks/ba-1.png" />
-        </div>
-        <div>
-            <img src="../img/mobile-mocks/ba-2.png" />
-        </div>
-        <div>
-            <img src="../img/mobile-mocks/ba-3.png" />
-        </div>
-    </Carousel> 
-);
+
+class HubbCarousel extends React.Component {
+  resize() {
+    if (window.matchMedia("(max-width : 768px)").matches) {
+        this.setState({ centerM: false });
+    } else {
+        this.setState({ centerM: true });
+    }
+  }
+  componentDidMount() {
+    window.addEventListener('resize', this.resize)
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize)
+  }
+  constructor(props) {
+    super(props)
+    if (window.matchMedia("(max-width : 768px)").matches) {
+        this.state = { centerM: false };
+    } else {
+        this.state = { centerM: true };
+    }
+
+    this.resize = this.resize.bind(this);
+  }
+  render() {
+    return  (
+        <Carousel className='hub-carousel' showThumbs={false} showStatus={false} emulateTouch={true} showArrows={false} centerMode={this.state.centerM} centerSlidePercentage={50} useKeyboardArrows={true}>
+            <div>
+                <img alt="hub-0" src="../img/mobile-mocks/hub-quiz-0.png" />
+            </div>
+            <div>
+                <img alt="hub-1" src="../img/mobile-mocks/hub-quiz-1.png" />
+            </div>
+        </Carousel> 
+    )}
+}
+
+class HubCarousel extends React.Component {
+  resize() {
+    if (window.matchMedia("(max-width : 768px)").matches) {
+        this.setState({ centerM: false });
+    } else {
+        this.setState({ centerM: true });
+    }
+  }
+  componentDidMount() {
+    window.addEventListener('resize', this.resize)
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize)
+  }
+  constructor(props) {
+    super(props)
+    if (window.matchMedia("(max-width : 768px)").matches) {
+        this.state = { centerM: false };
+    } else {
+        this.state = { centerM: true };
+    }
+    this.resize = this.resize.bind(this);
+  }
+  render() {
+    return  (
+        <Carousel showThumbs={false} showStatus={false} emulateTouch={true} showArrows={false} centerMode={this.state.centerM} centerSlidePercentage={50} useKeyboardArrows={true}>
+            <div>
+                <img src="../img/mobile-mocks/hub-0.png" />
+            </div>
+            <div>
+                <img src="../img/mobile-mocks/hub-1.png" />
+            </div>
+            <div>
+                <img src="../img/mobile-mocks/hub-2.png" />
+            </div>
+            <div>
+                <img src="../img/mobile-mocks/hub-3.png" />
+            </div>
+            <div>
+                <img src="../img/mobile-mocks/hub-4.png" />
+            </div>
+        </Carousel> 
+    )}
+}
+
+
+class BaCarousel extends React.Component {
+  resize() {
+    if (window.matchMedia("(max-width : 768px)").matches) {
+        this.setState({ centerM: false });
+    } else {
+        this.setState({ centerM: true });
+    }
+  }
+  componentDidMount() {
+    window.addEventListener('resize', this.resize)
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize)
+  }
+  constructor(props) {
+    super(props)
+    if (window.matchMedia("(max-width : 768px)").matches) {
+        this.state = { centerM: false };
+    } else {
+        this.state = { centerM: true };
+    }
+    this.resize = this.resize.bind(this);
+  }
+  render() {
+    return  (
+        <Carousel showThumbs={false} showStatus={false} emulateTouch={true} showArrows={false} centerMode={this.state.centerM} centerSlidePercentage={50} useKeyboardArrows={true}>
+            <div>
+                <img src="../img/mobile-mocks/ba-1.png" />
+            </div>
+            <div>
+                <img src="../img/mobile-mocks/ba-2.png" />
+            </div>
+            <div>
+                <img src="../img/mobile-mocks/ba-3.png" />
+            </div>
+        </Carousel>
+    )}
+}
+
  
 export { HubCarousel, HubbCarousel, BaCarousel };
